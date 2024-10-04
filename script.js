b0e3eb327a35ba533cc2a27e54b766a6a6aa475b
Veronika Teplova <teplovavo@gmail.com>
2024-10-04 12:40:23 -0700

    base code to start

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 console.log("Let's start!")
 
+console.log("Part 1: Humble Beginnings")
+
+
+    const adventurer = {
+        name: "Robin",
+        health: 10,
+        inventory: ["sword", "potion", "artifact"],
+        companion: {
+            name: "Leo",
+            type: "Cat"
+            companion: {
+                name: "Frank"
+                type: "Flea"
+                belongings: ["small hat", "sunglasses"]
+            }
+
+        }
+       
+        }
