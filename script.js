21b2a972835a83bd6ec8015eae813ff1d214bfe2
Veronika Teplova <teplovavo@gmail.com>
2024-10-04 13:00:02 -0700

    re-created using class, part2

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 console.log("Let's start!")
 
-console.log("Part 1: Humble Beginnings")
 
+
+////////////////////////////////////////////////////////////
+console.log("Part 1: Humble Beginnings")
 
 const adventurer = {
     name: "Robin",
@@ -23,3 +25,31 @@ const adventurer = {
     }
   };
   adventurer.roll();
+
+
+/////////////////////////////////////////////////////////////
+
+  console.log("Part 2: Class Fantasy")
+
+  class Character {
+    constructor (name) {
+      this.name = name;
+      this.health = 100;
+      this.inventory = [];
+    }
+    roll(mod = 0) {
+        const result = Math.floor(Math.random() * 20) + 1 + mod;
+        console.log(`${this.name} rolled a ${result}.`);
+      }
+  }
+
+  // re-create Robin using the Character class
+const robin = new Character("Robin");
+robin.inventory = ["sword", "potion", "artifact"];
+robin.companion = new Character("Leo");
+robin.companion.type = "Cat";
+robin.companion.companion = new Character("Frank");
+robin.companion.companion.type = "Flea";
+robin.companion.companion.inventory = ["small hat", "sunglasses"];
+
+robin.companion.roll();
