322225237130d3434aa74b50c38c9fc09e3128e6
Veronika Teplova <teplovavo@gmail.com>
2024-10-04 12:50:33 -0700

    Rolled method works

--- script.js
+++ script.js
@@ -3,19 +3,23 @@ console.log("Let's start!")
 console.log("Part 1: Humble Beginnings")
 
 
-    const adventurer = {
-        name: "Robin",
-        health: 10,
-        inventory: ["sword", "potion", "artifact"],
-        companion: {
-            name: "Leo",
-            type: "Cat"
-            companion: {
-                name: "Frank"
-                type: "Flea"
-                belongings: ["small hat", "sunglasses"]
-            }
-
-        }
-       
-        }
+const adventurer = {
+    name: "Robin",
+    health: 10,
+    inventory: ["sword", "potion", "artifact"],
+    companion: {
+      name: "Leo",
+      type: "Cat",
+      companion: {
+        name: "Frank",
+        type: "Flea",
+        inventory: ["small hat", "sunglasses"]
+      }
+    },
+    // Robin's dice roll method
+    roll(mod = 0) {
+      const result = Math.floor(Math.random() * 20) + 1 + mod;
+      console.log(`${this.name} rolled a ${result}.`);
+    }
+  };
+  adventurer.roll();
